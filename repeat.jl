f06cbed0ada6903c64f320106f6d8fef23eecb0c
ali tugrul balci <abalci16@ku.edu.tr>
2017-04-16 02:13:14 +0200

    final version

--- repeat.jl
+++ repeat.jl
@@ -2,7 +2,18 @@
 #     Pkg.installed(p) == nothing && Pkg.add(p)
 # end
 
-using Knet, JLD
+using Knet, JLD, ArgParse
+
+function parse_commandline()
+    s = ArgParseSettings()
+    @add_arg_table s begin
+        ("--epochs"; arg_type=Int; default=100; help="Number of epochs for training.")
+        ("--batchsize"; arg_type=Int; default=128; help="Number of sequences to train on in parallel.")
+        ("--lr"; arg_type=Float64; default=0.005; help="Initial learning rate.")
+        ("--weight"; arg_type=String; default=""; help="Initial weights instead of randomly initialized ones.")
+    end
+    return parse_args(s;as_symbols = true)        
+end
 
 function writeData(itxt,otxt,n)
   ofile = open(otxt, "w")
@@ -25,9 +36,9 @@ function prepData()
   isfile("data_te.seq") || download("https://cbcl.ics.uci.edu/public_data/DeepCons/data_te.seq", "data_te.seq")
   isfile("data_tr.seq") || download("https://cbcl.ics.uci.edu/public_data/DeepCons/data_tr.seq", "data_tr.seq")
   isfile("data_va.seq") || download("https://cbcl.ics.uci.edu/public_data/DeepCons/data_va.seq", "data_va.seq")
-  writeData("data_tr.seq","data_train.seq",600000)
-  writeData("data_te.seq","data_test.seq",150000)
-  writeData("data_va.seq","data_valid.seq",150000)
+  writeData("data_tr.seq","data_train.seq",1000000) #1290000
+  writeData("data_te.seq","data_test.seq",150000) #165000
+  writeData("data_va.seq","data_valid.seq",150000) #165000
 end
 
 function read_data()
@@ -58,6 +69,15 @@ end
 function seqMap(na)
   seqMap = Dict{Char,UInt8}('A' => 1, 'G' => 2, 'C' => 3, 'T' => 4)
   return seqMap[na]
+end
+
+function seqToVector(seq)
+  m = convert(Array{Float32}, zeros(4,200,1,1))
+  index = map(x-> sub2ind(size(m),x[1],x[2],1,1) , 
+              map(x -> (seqMap(x[2]),x[1]), 
+                filter(x -> x[2] != 'N', enumerate(seq))))
+  m[index] = 1
+  return m
 end
 
 function seqToVector!(matrix, seq, index)
@@ -91,26 +111,45 @@ function minibatch(x,y,sz)
     return data
 end
 
-function weights(;winit=0.1)
-    return Any[convert(KnetArray{Float32},randn(4,10,1,1000)*winit), 
-               convert(KnetArray{Float32},zeros(1,1,1000,1)),
-               convert(KnetArray{Float32},randn(4,20,1,500)*winit), 
-               convert(KnetArray{Float32},zeros(1,1,500,1)),
-               convert(KnetArray{Float32},randn(2,1500)*winit),
-               convert(KnetArray{Float32},zeros(2,1))]
+function weights(;w=[],winit=0.1)
+    if length(w) == 6
+      return Any[convert(KnetArray{Float32},w[1]), 
+                 convert(KnetArray{Float32},w[2]),
+                 convert(KnetArray{Float32},w[3]), 
+                 convert(KnetArray{Float32},w[4]),
+                 convert(KnetArray{Float32},w[5]),
+                 convert(KnetArray{Float32},w[6])]
+    else
+      return Any[convert(KnetArray{Float32},randn(4,10,1,1000)*winit), 
+                 convert(KnetArray{Float32},zeros(1,1,1000,1)),
+                 convert(KnetArray{Float32},randn(4,20,1,500)*winit), 
+                 convert(KnetArray{Float32},zeros(1,1,500,1)),
+                 convert(KnetArray{Float32},randn(2,1500)*winit),
+                 convert(KnetArray{Float32},zeros(2,1))]
+    end
 end
 
-function initparams(weights;learning_rate=0.005)
-  return map(x -> Adam(;lr=learning_rate), weights)
+function initparams(weights;learningRate=0.005)
+  return map(x -> Adagrad(;lr=learningRate), weights)
 end
 
 function predict(w,x)
     pool_1 = pool(dropout(relu(conv4(w[1],x) .+ w[2]), 0.25);stride=191,window=191)
+    pool_2 = pool(dropout(relu(conv4(w[3],x) .+ w[4]), 0.25);stride=181,window=181)
+    pool_1 = reshape(pool_1, (size(pool_1,1)*size(pool_1,2)*size(pool_1,3),size(pool_1,4)))
+    pool_2 = reshape(pool_2, (size(pool_2,1)*size(pool_2,2)*size(pool_2,3),size(pool_2,4)))
+    pool_out = [pool_1 ; pool_2]
+    y = dropout(w[5]*pool_out .+ w[6], 0.5)
+    return y
+end
+
+function pred(w,x)
+    pool_1 = pool(relu(conv4(w[1],x) .+ w[2]);stride=191,window=191)
     pool_2 = pool(relu(conv4(w[3],x) .+ w[4]);stride=181,window=181)
     pool_1 = reshape(pool_1, (size(pool_1,1)*size(pool_1,2)*size(pool_1,3),size(pool_1,4)))
     pool_2 = reshape(pool_2, (size(pool_2,1)*size(pool_2,2)*size(pool_2,3),size(pool_2,4)))
     pool_out = [pool_1 ; pool_2]
-    y = dropout(relu(w[5]*pool_out .+ w[6]), 0.5)
+    y = w[5]*pool_out .+ w[6]
     y = y .- maximum(y,1)
     expsum = sum(exp(y),1)
     pred = exp(y)./expsum
@@ -118,8 +157,11 @@ function predict(w,x)
 end
 
 function loss(w,x,ygold)
-    pred_y = predict(w,x)
-    lost = -sum(ygold .*pred_y) / size(x, 4)
+    y = predict(w,x)
+    y = y .- maximum(y,1)
+    expy = exp(y)
+    logphat = y .- log(sum(expy,1))
+    lost = -sum(ygold .* logphat) / size(x, 4)
     return lost
 end
 
@@ -144,7 +186,7 @@ function avgloss(w,data)
     return sum, cnt
 end
 
-function accuracy(w,dtst,pred=predict)
+function accuracy(w,dtst)
     ncorrect = 0
     ninstance = 0
     nloss = 0
@@ -158,17 +200,25 @@ function accuracy(w,dtst,pred=predict)
     return (ncorrect, nloss,ninstance)
 end
 
-function main()
+function main(args=ARGS)
+  opts = parse_commandline()
+  println("opts=",[(k,v) for (k,v) in opts]...)
   prepData()
   chunk_size = 12800
-  w = weights()
-  params = initparams(w)
+  if opts[:weight] != ""
+    println("reading initial weight from $(opts[:weight]) file.")
+    w = weights(;w=load(opts[:weight])["w"])
+  else
+    w = weights()
+  end
+  params = initparams(w;learningRate=opts[:lr])
   dtrain,dtest,dvalid = read_data()
   itrain, ival, itest = getIter(length(dtrain),length(dtest),length(dvalid),chunk_size)
   patience = 0
-  bests = 0
+  bests = Inf
   bestw = Any[]
-  @time for epoch=1:100
+  validLost = Any[]
+  @time for epoch=1:opts[:epochs]
     if patience > 10
       print("Early stopping no progess for 10 epoch... \n")
       break
@@ -178,22 +228,21 @@ function main()
     @time for i=1:itrain
       data = getChunk(dtrain, chunk_size, i)
       xtrn, ytrn = preprocess(data)
-      dtrn = minibatch(xtrn,ytrn,128)
+      dtrn = minibatch(xtrn,ytrn,opts[:batchsize])
       train(w,dtrn,params)
-      gc()
     end
     nloss = 0;ninstance = 0
     tloss = 0;tinstance = 0
     for i=1:ival
       data = getChunk(dvalid, chunk_size, i)
       xva, yva = preprocess(data)
-      dva = minibatch(xva,yva,128)
+      dva = minibatch(xva,yva,opts[:batchsize])
       nloss, ninstance= avgloss(w,dva)
       tloss += nloss; tinstance += ninstance
       xva = 0;yva = 0;data = 0;dva = 0;
-      gc()
     end
     average_loss = tloss/tinstance
+    push!(validLost,average_loss)
     print("$epoch: validation lost is $(average_loss) \n")
     if average_loss < bests
       bestw = w
@@ -207,15 +256,15 @@ function main()
   for i=1:itest
     data = getChunk(dtest, chunk_size, i)
     xtst, ytst = preprocess(data)
-    dtst = minibatch(xtst,ytst,128)
+    dtst = minibatch(xtst,ytst,opts[:batchsize])
     ncorr, nwrong, ninstance = accuracy(bestw,dtst)
     corr += ncorr; wrong += nwrong; instance += ninstance
     xtst = 0;ytst = 0;data = 0;dtst = 0;
-    gc()
   end
   print("Test accuracy is $(corr/instance) $(wrong/instance) \n")
   print("Writing the weights of the best model to weights.jld \n")
-  save("weights.jld","w",map(x->convert(Array{Float32},x),bestw))
+  save("weights.jld","w",map(x->convert(Array{Float32},x),bestw),"J",validLost,"s",bests)
+  return bestw
 end
 
-main()
+w = main()
